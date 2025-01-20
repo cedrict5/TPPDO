7722f951ad2665e45d889b77651863b7761c56f8
cedric <cedrictiana78@gmail.com>
2025-01-20 09:59:31 +0100

    ajout include head foot

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
   </head>
   <body>
     
-
+<?php include('header.php');?>
 
 <main role="main">
 
@@ -124,13 +124,4 @@
 
 </main>
 
-<footer class="container">
-  <p>&copy; Company 2017-2024</p>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/f52ca63eb6.js" crossorigin="anonymous"></script>
-      
-  </body>
-</html>
+<?php include('footer.php');?>
