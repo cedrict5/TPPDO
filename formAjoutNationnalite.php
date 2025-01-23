f45dd3de3abef522dcebca869aa7b8be0abe7cca
cedric <cedrictiana78@gmail.com>
2025-01-23 13:53:45 +0100

    création form Ajout Nationnalité

--- formAjoutNationnalite.php
+++ formAjoutNationnalite.php
@@ -1,57 +1,4 @@
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.101.0">
-    <title>Jumbotron Template · Bootstrap v4.6</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/jumbotron/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.css">
-
-
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/4.6/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/4.6/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/4.6/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/4.6/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/4.6/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-<link rel="icon" href="/docs/4.6/assets/img/favicons/favicon.ico">
-<meta name="msapplication-config" content="/docs/4.6/assets/img/favicons/browserconfig.xml">
-<meta name="theme-color" content="#563d7c">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="jumbotron.css" rel="stylesheet">
-  </head>
-  <body>
-    
 <?php include('header.php');
 
 ?>
@@ -59,6 +6,19 @@
 
 <div class="containner mt-5">
 
+<form action="valideAjoutNationnalite.php" method="post">
+    <div class="form-group">
+        <label for="libelle"> Libellé </label>
+        <input type="text" class="form-controle" id="libelle" placeholder="Saisir le libellé" name="libelle">
+    </div>
+
+<div class="row">
+    <div class="col"> <a href="listeNationnalite.php" class="bt btn-warning"> </div>
+<div class="col"><button type="submit" class="btn btn-success"> Ajouter </button></div>
+</div>
+</form>
+
 </div>
 
 <?php include('footer.php');?>
+>
